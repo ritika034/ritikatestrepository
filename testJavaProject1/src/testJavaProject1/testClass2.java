a0af17d28ea583e7fefa9dc74e75c16517f23d69
Ritika Mehta <ritzm03@gmail.com>
2020-03-24 16:07:32 +0530

    Commit change in testClass2.java

--- testJavaProject1/src/testJavaProject1/testClass2.java
+++ testJavaProject1/src/testJavaProject1/testClass2.java
@@ -1,8 +1,10 @@
+//ver1.1 common change on branch2
 package testJavaProject1;
 
 public class testClass2 {
  void testClass2(int a,int b)
  {
   System.out.println("Hey! I am a constructor of testClass2");
+  System.out.println("Hey! I am common change on branch2");
  }
 }
