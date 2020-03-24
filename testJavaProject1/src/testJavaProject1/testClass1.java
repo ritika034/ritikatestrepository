f1e931a8bc0f06d9c838f6a1458b8110aa58c0d1
Ritika Mehta <ritzm03@gmail.com>
2020-03-24 16:38:57 +0530

    Changes in testClass1 for fix1

--- testJavaProject1/src/testJavaProject1/testClass1.java
+++ testJavaProject1/src/testJavaProject1/testClass1.java
@@ -1,12 +1,14 @@
 //ver 1.1 Fix1
 package testJavaProject1;
 
+//ver1.1 For fix1
 public class testClass1 {
 	public static void main(String args[])
 	{
 		System.out.println("Hey! Good morning");
 		System.out.println("First change after 1st commit");
 		System.out.println("Hi This is branch1");
+		System.out.println("Hey I am ver1.1 for fix1");
 	}
 
 }
