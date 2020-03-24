80ad07cc352211cf86d0eb96c3dbab8699461f04
Ritika Mehta <ritzm03@gmail.com>
2020-03-24 16:37:57 +0530

    Changes in testClass1 to test merge tool

--- testJavaProject1/src/testJavaProject1/testClass1.java
+++ testJavaProject1/src/testJavaProject1/testClass1.java
@@ -1,3 +1,4 @@
+//ver1.1 to test merge tool
 package testJavaProject1;
 
 public class testClass1 {
@@ -5,6 +6,7 @@ public class testClass1 {
 	{
 		System.out.println("Hey! Good morning");
 		System.out.println("First change after 1st commit");
+		System.out.println("Hey I am ver1.1 to test merge tool");
 	}
 
 }
