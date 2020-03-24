06c4c664455884d1762a816ce0dd1aee8a3110ca
Ritika Mehta <ritzm03@gmail.com>
2020-03-24 16:40:01 +0530

    Changes in testClass2 for fix2

--- testJavaProject1/src/testJavaProject1/testClass2.java
+++ testJavaProject1/src/testJavaProject1/testClass2.java
@@ -1,5 +1,5 @@
 //ver1.1 common change on branch1 and common change on branch2
-
+//ver1.2 for fix2
 package testJavaProject1;
 public class testClass2 {
  void testClass2(int a,int b)
@@ -9,6 +9,8 @@ public class testClass2 {
   System.out.println("Hey! I am a common change on branch1");
 
   System.out.println("Hey! I am common change on branch2");
+  
+  System.out.println("Hey! I am fix2");
 
  }
 }
