4da94ba22dd77ecf9cd5e74213c48a7c331df13f
Ritika Mehta <ritzm03@gmail.com>
2020-03-24 16:41:27 +0530

    Changes in testClass2 for fix3

--- testJavaProject1/src/testJavaProject1/testClass2.java
+++ testJavaProject1/src/testJavaProject1/testClass2.java
@@ -1,8 +1,11 @@
+//ver1.1 I am fix3
 package testJavaProject1;
 
 public class testClass2 {
  void testClass2(int a,int b)
  {
   System.out.println("Hey! I am a constructor of testClass2");
+  
+  System.out.println("Hey! I am fix3");
  }
 }
